49a3a96a0247fc693d2e43636f45348a0669f539
yenling-wong <107708053+yenling-wong@users.noreply.github.com>
2022-06-20 21:10:53 +0200

    Add files via upload

---
+++ arrays/1313. Decompress Run-Length Encoded List/solution.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> decompressRLElist(vector<int>& nums) {
+    vector<int> decompressed;
+
+    int n = nums.size();
+
+    for (int i = 0; i < n;  i = i + 2) {
+        for (int j = 0; j < nums[i]; j++){
+            decompressed.push_back(nums[i + 1]);
+        }
+    }
+    return decompressed;
+}
+
+void printVector(vector<int>& nums) {
+    for (size_t i = 0; i < nums.size(); i++) {
+        cout << nums[i] << " ";
+    }
+}
+
+int main() {
+    vector<int> nums = {1, 1, 2, 3};
+    vector<int> result = decompressRLElist(nums);
+    printVector(result);
+    return 0;
+}
