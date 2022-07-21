5ebd4b857db4ddafa6a9a219d1d56af3fcc38303
yenling-wong <107708053+yenling-wong@users.noreply.github.com>
2022-07-21 20:30:40 +0200

    Update optimized-2.cpp

--- arrays/2006. Count Number of Pairs With Absolute Difference K/optimized-2.cpp
+++ arrays/2006. Count Number of Pairs With Absolute Difference K/optimized-2.cpp
@@ -7,8 +7,9 @@ using namespace std;
 int countKDifference(vector<int>& nums, int k) {
     int ctr = 0;
     unordered_map<int,int> mp;
+    
     for(int i=0;i<nums.size();i++){
-        ctr+=mp[nums[i] + k] + mp[nums[i] - k];
+        ctr += mp[nums[i] + k] + mp[nums[i] - k];
         mp[nums[i]]++;
     }
     return ctr;
