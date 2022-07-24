2a329be36e6a4c35727c36b8de15fe74538e84fa
yenling-wong <107708053+yenling-wong@users.noreply.github.com>
2022-07-24 09:29:34 +0200

    Add files via upload

---
+++ arrays/1725. Number Of Rectangles That Can Form The Largest Square/solution.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int countGoodRectangles(vector<vector<int>>& rectangles) {
+    map<int, int> m;
+    
+    for (size_t i = 0; i < rectangles.size(); i++) {
+        int minimum = min(rectangles[i][0], rectangles[i][1]);
+        m[minimum]++;
+    }
+    
+
+    return m.rbegin()->second;
+}
+
+int main() {
+    vector<vector<int>> rectangles = {{5, 8}, {3, 9}, {5, 12},{16, 5}};
+    cout << countGoodRectangles(rectangles);
+    return 0;
+}
