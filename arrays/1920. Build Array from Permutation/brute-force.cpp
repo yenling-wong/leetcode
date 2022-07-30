b79e3c66c32b5caeefddafebb7b6ccfbe6fbc612
yenling-wong <107708053+yenling-wong@users.noreply.github.com>
2022-07-30 22:16:55 +0200

    Update brute-force.cpp

--- arrays/1920. Build Array from Permutation/brute-force.cpp
+++ arrays/1920. Build Array from Permutation/brute-force.cpp
@@ -20,6 +20,7 @@ void printVector(vector<int>& ans) {
         cout << ans[i] << " ";
     }
 }
+// Helper Function for Printing Result
 
 int main() {
     vector<int> nums = {0, 2, 1, 5, 3, 4};
@@ -29,5 +30,6 @@ int main() {
     return 0;
 }
 
-
-
+/* Solution Explanation: Create a new vector. For each iteration, add nums[nums[i]] to the vector. */
+/* Time Complexity: O(n), since we run through each value in the array exactly once. */
+/* Space Complexity: O(n), since we create a new vector of exactly n elements. This can be improved by modifying the elements in-situ. See optimized version. */
