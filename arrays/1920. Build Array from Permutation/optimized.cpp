0f64f92bbe58365766d772106f15f4793c731610
yenling-wong <107708053+yenling-wong@users.noreply.github.com>
2022-07-31 12:56:25 +0200

    Update optimized.cpp

--- arrays/1920. Build Array from Permutation/optimized.cpp
+++ arrays/1920. Build Array from Permutation/optimized.cpp
@@ -16,8 +16,9 @@ vector<int> buildArray(vector<int>& nums, int n) {
     return nums;
         
 }
-/* We know that (a + b * n) / n = b and (a + b * n) % n = a . In this context, we let a = nums[i] and b = nums[nums[i]]. In the first iteration, you add b * n to a. 
-Since the number might have been modified to nums[nums[i]], we apply the % operation to obtain the original nums[i]. */
+/* Solution Description: We know that (a + b * n) / n = b and (a + b * n) % n = a . In this context, we let a = nums[i] and b = nums[nums[i]]. In the first iteration, 
+we add nums[nums[i]] to nums[i]. Since the number might have been modified to nums[nums[i]] already, we apply the % operation to obtain the original nums[i]. 
+In the second iteration, we divide all the elements in the array by n to obtain nums[nums[i]]. */
 
 void printVector(vector<int>& ans) {
     for (size_t i = 0; i < ans.size(); i++) {
@@ -34,3 +35,6 @@ int main() {
 
     return 0;
 }
+
+// Time Complexity: O(n), since we iterate through the full array twice.
+// Space Complexity: O(1), since we modify the values in-situ.
