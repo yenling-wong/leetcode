7963fae05cfd1f65b7f42160fe3242af5f317c66
yenling-wong <107708053+yenling-wong@users.noreply.github.com>
2022-07-31 16:22:05 +0200

    Update solution.cpp

--- arrays/2011. Final Value of Variable After Performing Operations/solution.cpp
+++ arrays/2011. Final Value of Variable After Performing Operations/solution.cpp
@@ -15,9 +15,13 @@ int finalValueAfterOperations(vector<string>& operations) {
     return value;
     
 }
+/* A variable is initialized to store the value of the integer, starting with zero. In the if statement, if an increment operation is read, then
+the value increments by 1, and vice versa. */
 
 int main() {
     vector<string> operations = {"--X","X++","X++"};
     cout << finalValueAfterOperations(operations);
     return 0;
 }
+// Time Complexity: O(n), the string vector operations is iterated through once.
+// Space Complexity: O(1), no extra space needed besides the new int variable.
