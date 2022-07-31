adb266673474c4c956125dc7ea4e405886c2df72
yenling-wong <107708053+yenling-wong@users.noreply.github.com>
2022-07-31 16:16:21 +0200

    Update solution.cpp

--- arrays/1480. Running Sum of 1d Array/solution.cpp
+++ arrays/1480. Running Sum of 1d Array/solution.cpp
@@ -10,12 +10,16 @@ vector<int> runningSum(vector<int>& nums) {
         
     return nums;      
 }
+/* Printing the prefix sum of the array, which is to add the result of an element to the next element in the array. This is a common
+technique for solving array problems. */
 
 void printVector(vector<int>& ans) {
     for (size_t i = 0; i < ans.size(); i++) {
         cout << ans[i] << " ";
     }
 }
+
+// Helper Function to Print Resulting Vector
 
 int main() {
     vector<int> nums = {0, 2, 1, 5, 3, 4};
@@ -25,5 +29,5 @@ int main() {
     return 0;
 }
 
-
-
+// Time Complexity: O(n), the array is iterated through once.
+// Space Complexity: O(1), not extra space is needed because the operations are performed in-situ.
