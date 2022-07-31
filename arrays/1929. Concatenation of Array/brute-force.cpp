b70a92b37abf1363b24493e066803e3aef76c03e
yenling-wong <107708053+yenling-wong@users.noreply.github.com>
2022-07-31 13:23:39 +0200

    Update brute-force.cpp

--- arrays/1929. Concatenation of Array/brute-force.cpp
+++ arrays/1929. Concatenation of Array/brute-force.cpp
@@ -15,6 +15,9 @@ vector<int> getConcatenation(vector<int>& nums) {
     return ans;
 }
 
+/* We create a new array. We use two for loops to push elements onto the new array, and quit the while loop when the number of elements are twice
+the number of the original array. */
+
 void printVector(vector<int>& vec) {
     for (size_t i = 0; i < vec.size(); i++) {
         cout << vec[i] << " ";
@@ -27,3 +30,6 @@ int main() {
     printVector(ans);
     return 0;
 }
+
+// Time Complexity: O(n), we iterate over the nums array twice.
+// Space Complexity: O(n), we create a new array of twice the size of the original array.
