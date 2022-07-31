0bf85d4c429b69eba48f4a56e8cd8a5dc6512599
yenling-wong <107708053+yenling-wong@users.noreply.github.com>
2022-07-31 16:46:17 +0200

    Update brute-force.cpp

--- arrays/1470. Shuffle the Array/brute-force.cpp
+++ arrays/1470. Shuffle the Array/brute-force.cpp
@@ -6,7 +6,7 @@ using namespace std;
 vector<int> shuffle(vector<int>& nums, int n) {
     vector<int> ans;
 
-    for (size_t i = 0; i < nums.size(); i++) {
+    for (size_t i = 0; i < nums.size() / 2; i++) {
         ans.push_back(nums[i]);
         ans.push_back(nums[n + i]);
     }
@@ -14,11 +14,15 @@ vector<int> shuffle(vector<int>& nums, int n) {
     return ans;
 }
 
+/* Create a new vector. Iterate halfway through the nums array, push both the values of the current index and that plus n. */
+
 void printVector(vector<int>& nums) {
     for (size_t i = 0; i < nums.size(); i++) {
         cout << nums[i] << " ";
     }
 }
+
+// Helper Function to Print the Result
 
 int main() {
     vector<int> nums = {2,5,1,3,4,7};
@@ -29,3 +33,6 @@ int main() {
     printVector(nums);
     return 0;
 }
+
+// Time Complexity: O(n), because we only traverse through half the array.
+// Space Complexity: O(n), a new vector of n length was created to store the values.
