ebfe1845141e63df7bc58ef456d96774c93ae171
yenling-wong <107708053+yenling-wong@users.noreply.github.com>
2022-08-01 19:44:17 +0200

    Add files via upload

---
+++ arrays/2206. Divide Array Into Equal Pairs/map.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        map<int, int> m;
+        for (int i = 0; i < nums.size(); i++) {
+            m[nums[i]]++;
+        }
+        
+        for (auto it = m.begin(); it != m.end(); it++) {
+            if ((it->second % 2) == 1) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
