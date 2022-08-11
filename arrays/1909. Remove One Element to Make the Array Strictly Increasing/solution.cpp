61191c720d4b7d73500866a11dfa3b1755f9f5b9
yenling-wong <107708053+yenling-wong@users.noreply.github.com>
2022-08-11 11:17:17 +0200

    Update solution.cpp

--- arrays/1909. Remove One Element to Make the Array Strictly Increasing/solution.cpp
+++ arrays/1909. Remove One Element to Make the Array Strictly Increasing/solution.cpp
@@ -30,12 +30,13 @@ bool canBeIncreasing(vector<int>& nums) {
 }
 
 /*We keep two counters, one for a left scan of the array, and one for the right. We also keep a 'last' pointer 
-to track the previous element as we traverse the array. Should the current element be strictly greater than its previous, 
-we update the last pointer to the current element. Else, we increment the leftcount variable for each consecutive element 
-that is not stricly greater than an element to its left. Vice versa for a right scan of the array. The left and right counters
-determine the number of consecutive elements that are not strictly greater (or lesser) than any one of the elements to their left (or right).
-It only works if both left and right scans are performed on the array, and will return true if one of the counters are
-either one (only one element should be removed), or zero (already strictly increasing). */
+to track the previous element as we traverse the array. Should the current element be strictly greater than its 
+previous, we update the last pointer to the current element. Else, we increment the leftcount variable for each 
+consecutive element that is not stricly greater than an element to its left. Vice versa for a right scan of the 
+array. The left and right counters determine the number of consecutive elements that are not strictly greater 
+(or lesser) than any one of the elements to their left (or right). It only works if both left and right scans 
+are performed on the array, and will return true if one or both of the counters are either one (only one element 
+should be removed), or zero (already strictly increasing). */
 
 int main() {
     vector<int> nums = {1, 2, 10, 5, 7};
