b7475fe24906801fb7b149060bb669c9ce1dd6b7
yenling-wong <107708053+yenling-wong@users.noreply.github.com>
2022-08-11 12:08:06 +0200

    Update counter.cpp

--- arrays/1909. Remove One Element to Make the Array Strictly Increasing/counter.cpp
+++ arrays/1909. Remove One Element to Make the Array Strictly Increasing/counter.cpp
@@ -12,7 +12,6 @@ bool canBeIncreasing(vector<int>& nums) {
                 return false;
             }
             count++;
-            // Removing previous element.
 
             if (i > 1 && nums[i] <= nums[i - 2]) {
                 nums[i] = nums[i - 1];
@@ -24,11 +23,10 @@ bool canBeIncreasing(vector<int>& nums) {
     return true;
 }
 /* We keep a counter to track the number of elements that are not strictly greater than the element
-to their left. When we increment the counter, it is equivalent to replacing the previous element. 
-On line 16, we replace the current element with its previous element if it is still smaller than the 
-element two spaces before it. This is equivalent to removing the current element, and ensuring that 
-the following element is not compared to that value, but the value before it. Without this mechanism 
-in place, the array {6, 7, 2, 3, 9} will pass. */
+to their left. On line 16, we replace the current element with its previous element if it is still 
+smaller than the element two spaces before it. This is equivalent to removing the current element, 
+and ensuring that the following element is not compared to that value, but the value before it. 
+Without this mechanism in place, the array {6, 7, 2, 3, 9} will pass. */
 
 int main() {
     vector<int> nums = {6, 7, 2, 8, 9};
