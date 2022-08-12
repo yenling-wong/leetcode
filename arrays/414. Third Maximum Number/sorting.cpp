5e6bf451fd8303445ae961c60e0a1d40fd8cbdda
yenling-wong <107708053+yenling-wong@users.noreply.github.com>
2022-08-12 19:43:49 +0200

    Update sorting.cpp

--- arrays/414. Third Maximum Number/sorting.cpp
+++ arrays/414. Third Maximum Number/sorting.cpp
@@ -20,11 +20,16 @@ int thirdMax(vector<int>& nums) {
     return nums.back();      
 }
 
+/* We sort the array in ascending order, and we perform a right scan of the array. We keep track of the distinct 
+elements by incrementing the counter. As soon as the counter equals three, we return that element as it is the third 
+maximum. If there is no such element, we simply return the last element of the array which is the largest after 
+sorting. */
+
 int main() {
     vector<int> nums = {2, 2, 3, 1};
     cout << thirdMax(nums);
     return 0;
 }
 
-// Time Complexity: O(n log n)
-// Space Complexity:
+// Time Complexity: O(n log n) + O(n) = O(n log n). Sorting the array takes n log n time, then we perform another traversal of the array.
+// Space Complexity: O(1), no extra space needed, as the sorting is done in place.
