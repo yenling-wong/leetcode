035ff750e7bf0ec7c547631b443f476204152dd7
yenling-wong <107708053+yenling-wong@users.noreply.github.com>
2022-08-12 20:20:38 +0200

    Create set.cpp

---
+++ arrays/414. Third Maximum Number/set.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int thirdMax(vector<int>& nums) {
+    set<int> st;
+    int n = nums.size(), count = 0;
+
+    for (int i = 0; i < n; i++) {
+        st.insert(nums[i]);
+    }
+
+    for (auto it = st.rbegin(); it != st.rend(); it++) {
+        count++;
+
+        if (count == 3) {
+            return *it;
+        }
+    }
+
+    auto itr = st.rbegin();
+
+    return *itr;
+}
+
+int main() {
+    vector<int> nums = {2, 2, 3, 1};
+    cout << thirdMax(nums);
+    return 0;
+}
+
+// Time Complexity: O(log n) + O(n) = O(n), the insertion operation for a set takes log n time where n is the number of elements in the set. 
+// Since we iterate through the set to obtain the third maximum, it takes linear time, which is upper-bounded by O(n) because the array could 
+// contain all distinct elements.
+// Space Complexity: O(n), We use a set to store all distinct elements, so it is upper bounded by O(n).
