9ec0eb0f169574a8d56c7f05f721e7542d5c88e3
yenling-wong <107708053+yenling-wong@users.noreply.github.com>
2022-08-12 16:33:08 +0200

    Update heap.cpp

--- arrays/414. Third Maximum Number/heap.cpp
+++ arrays/414. Third Maximum Number/heap.cpp
@@ -28,14 +28,14 @@ int thirdMax(vector<int>& nums) {
 }
 
 /*We use a priority queue here to store elements in descending order (usually the largest element gets the highest priority,
-but we change the ordering with the extra parameters). As such, the element with the highest priority (smallest) gets
-served first, and the top element is the element with the highest priority (the smallest). We use a set to ensure only unique 
-values get pushed into the queue. The count function for the set STL returns 1 if the element is already in the set, and 0 if
-it isn't. When the element gets pushed on to the queue, the element is assigned priority based on its magnitude. If we restrict
-the elements to only a maximum size of three, we will always pop the lowest element (has the highest priority) so that only the 
-top three unique largest elements remain in the queue. If the third maximum does not exist and the queue only has two elements,
-we pop the second maximum, leaving us with the maximum, else we return the third largest element in the queue, which is also 
-the smallest element at the top. If there only exist one element, then that element is simply the maximum. */
+but we change the ordering with the extra parameters). As such, the element with the highest priority (smallest) gets served 
+first, and the top element is the element with the highest priority (the smallest). We use a set to ensure only unique values 
+get pushed into the queue. The count function for the set STL returns 1 if the element is already in the set, and 0 if it isn't. 
+When the element gets pushed on to the queue, the element is assigned priority based on its magnitude. If we restrict the 
+elements to only a maximum size of three, we will always pop (dequeue) the lowest element (has the highest priority) so that 
+only the top three unique largest elements remain in the queue. If the third maximum does not exist and the queue only has two 
+elements, we pop the second maximum, leaving us with the maximum, else we return the third largest element in the queue, which 
+is also the smallest element at the top. If there only exist one element, then that element is simply the maximum. */
 
 int main() {
     vector<int> nums = {2, 2, 3, 1};
