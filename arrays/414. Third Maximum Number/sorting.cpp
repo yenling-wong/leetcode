e491e679040698ebe98376e8202912404e9b6cb4
yenling-wong <107708053+yenling-wong@users.noreply.github.com>
2022-08-12 16:56:44 +0200

    Update sorting.cpp

--- arrays/414. Third Maximum Number/sorting.cpp
+++ arrays/414. Third Maximum Number/sorting.cpp
@@ -1,4 +1,32 @@
-// Pending
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int thirdMax(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    int count = 1, n = nums.size(), last = nums.back();
+
+    for (int i = n - 1; i >= 0; i--) {
+        if (last != nums[i]) {
+            last = nums[i];
+            count++;
+        }
+
+        if (count == 3) {
+            return nums[i];
+        }
+    }
+    
+    return nums.back();      
+}
+
+int main() {
+    vector<int> nums = {2, 2, 3, 1};
+    cout << thirdMax(nums);
+    return 0;
+}
 
 // Time Complexity: O(n log n)
 // Space Complexity:
